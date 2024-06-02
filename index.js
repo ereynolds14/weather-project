60676d347e8a1ed27e7d24e664d8d05323eb0e2a
ereynolds14 <168741998+ereynolds14@users.noreply.github.com>
2024-06-02 11:58:13 -0600

    API call for current temp
    Added API functionality to search for city's current temperature

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
-function citySearch(city) {
-    let apiKey ="fd09t4eabb5a4a07bf90fbe0e2oe3ae5";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial"`;
-    console.log(apiUrl);
 
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    
 }
 
 
 
+
+function citySearch(city) {
+    let apiKey ="fd09t4eabb5a4a07bf90fbe0e2oe3ae5";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(refreshWeather);
+
+}
 
 
 function handleSearchSubmit(event) {
@@ -23,6 +35,6 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector('.search-form');
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-
+citySearch("Denver");
 
 
