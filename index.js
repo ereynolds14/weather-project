b22ed155bedf391b21eeb6b40341307c2434fe66
ereynolds14 <168741998+ereynolds14@users.noreply.github.com>
2024-06-02 11:10:34 -0600

    fixed function name
    fixed function name and changed index.js to call by id name not class

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 
+function citySearch(city) {
+    let apiKey ="fd09t4eabb5a4a07bf90fbe0e2oe3ae5";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial"`;
+    console.log(apiUrl);
+
+}
+
+
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector('.search-form-input');
-    let cityElement = document.querySelector('.weather-app-city');
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
      cityElement.innerHTML = searchInput.value;
+     citySearch(searchInput.value);
 
 }
 
