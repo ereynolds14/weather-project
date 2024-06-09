0effbc9d2671309253b52dd20d0fcf15780dec50
ereynolds14 <168741998+ereynolds14@users.noreply.github.com>
2024-06-09 16:33:24 -0600

    styling updates
    changed temp forecast font size and color

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function displayForecast(response) {
     <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
     <div class="weather-forecast-temperatures">   
-       <span class="weather-forecast-temperature-max">${Math.round(day.temperature.maximum)}°</span>  
+       <span class="weather-forecast-temperature-max">${Math.round(day.temperature.maximum)}°    </span>  
        <span class="weather-forecast-temperature-min">${Math.round(day.temperature.minimum)}°</span> 
     </div>
 </div>
